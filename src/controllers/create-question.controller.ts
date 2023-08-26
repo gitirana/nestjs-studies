d0713705953a8469c9f09728e86662524c825710
gitirana <thaygitirana@gmail.com>
2023-08-26 13:12:23 -0300

    feat: create current user decorator

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,21 +1,15 @@
-import { Controller, Post, UseGuards, UsePipes } from "@nestjs/common";
-import { AuthGuard } from "@nestjs/passport";
+import { Controller, Post, UseGuards } from "@nestjs/common";
+import { CurrentUser } from "src/auth/current-user.decorator";
 import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
-import { ZodValidationPipe } from "src/pipes/zod-validation.pipe";
-import { z } from "zod";
-
-const createQuestionBodySchema = z.object({
-  email: z.string().email(),
-  password: z.string(),
-});
-
-type CreateQuestionSchema = z.infer<typeof createQuestionBodySchema>;
+import { UserPayload } from "src/auth/jwt.strategy";
 
 @Controller("/questions")
 @UseGuards(JwtAuthGuard)
 export class CreateQuestionController {
   @Post()
-  async handle() {
+  async handle(@CurrentUser() user: UserPayload) {
+    console.log(user);
+
     return "ok";
   }
 }
